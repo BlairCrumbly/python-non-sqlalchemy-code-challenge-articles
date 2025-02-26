6a4eca09f108422f79f0d806ef4426306892be0c
Blair Crumbly <blaircrumbly@gmail.com>
2025-02-26 17:48:42 -0500

    all Article tests passing

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,9 +1,31 @@
 class Article:
+    all = []
+
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
-        self.title = title
-        
+        #since theres no setter. validate in order to set the tit
+        #*this works the too: type(title) == str
+        if isinstance(title, str) and 5 <= len(title) <= 50:
+            self._title = title
+        else:
+            raise TypeError("Title must be a string between 5 and 50 characters")
+        type(self).all.append(self)
+    
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, new_value):
+        #? ignore attempts to change the title, only solution that works
+        pass
+    
+    def get_title(self):
+        print(self._title)
+
+
+
 class Author:
     def __init__(self, name):
         self.name = name
